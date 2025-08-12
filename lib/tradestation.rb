e14e646717f80a48357bb9c922e4da4ce20721a6
Ryan Hamamura <58859899+ryanhamamura@users.noreply.github.com>
2025-08-11 15:46:12 -1000

    Initial gem scaffold
    - Ruby gem structure with bundler
    - MIT license
    - RSpec testing framework
    - GitHub Actions CI
    - RuboCop linting configured

---
+++ lib/tradestation.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tradestation/version"
+
+module Tradestation
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/tradestation_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Tradestation do
+  it "has a version number" do
+    expect(Tradestation::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
